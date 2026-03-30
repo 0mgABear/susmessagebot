430b1ab15279029c00054a32f1f998f02adcdc2e
0mgABear <yeoyi94@gmail.com>
2026-03-30 23:38:04 +0800

    Minor addition of variable

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+WEBHOOK_URL = "https://susmessagebot.commonertech.dev"
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GROQ_MODEL = "llama-3.1-8b-instant"
 EMBEDDING_MODEL = "all-MiniLM-L6-v2"
