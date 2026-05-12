53aed6d05bc718cfde8dfc208ec7bafa74c72844
YY <106061169+0mgABear@users.noreply.github.com>
2026-05-12 21:23:13 +0800

    Add BAN example via HITL feedback

--- seeds.py
+++ seeds.py
@@ -106,6 +106,7 @@ examples = [
     ('open for? @Xp_seRi', "BAN"),
     ('Check my story pm if also want fast cash payout', "BAN"),
     ('Urgent fast Job 2.9k check story if can\n\n @Juel15  urgent pm', "BAN"),
+    ('https://gofund.me/27b9a5b1b', "BAN"),
 ]
 
 
