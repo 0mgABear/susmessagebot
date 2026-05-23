f599db97038c396bf4c7e4daae34e968dae799b5
YY <106061169+0mgABear@users.noreply.github.com>
2026-05-23 15:56:09 +0800

    Add BAN example via HITL feedback

--- seeds.py
+++ seeds.py
@@ -126,6 +126,7 @@ examples = [
     ('Pm 2.7 now if int', "BAN"),
     ('Wrk urgent fast Job 2.9k check story if can\n\n urgent pm', "BAN"),
     ('@qui_ling\n\n\n\n\n\n\n@qui_ling', "BAN"),
+    ('Giv ing away my 15-inch MacBook Air (M4) — bareIy used and stiII in exceIIent condition.', "BAN"),
 ]
 
 
