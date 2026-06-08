68f688f892318a2bd445e62bffbbb5b62aacbd42
YY <106061169+0mgABear@users.noreply.github.com>
2026-06-08 20:55:38 +0800

    Add SAFE example via HITL feedback

--- seeds.py
+++ seeds.py
@@ -156,6 +156,7 @@ examples = [
     ('@Lee_Xin_Yi\n\n\n\n\n\n@Lee_Xin_Yi', "BAN"),
     ('Hi everyone,\n\nGi ving away 2 × 3 Sixty 16 inch foIdabIe bicycIes (chameIeon bIue & red) – both in good working condition.\n\nGreat for commuting or casuaI rides.\n\nIf you’re interes ted, feeI f r e e to D M.\n\nThanks.', "BAN"),
     ('@x_i_n_W_i_i\n\n\n\n\n\n\n\n@x_i_n_W_i_i', "BAN"),
+    ('scam', "SAFE"),
 ]
 
 
