854c37f34b994d1d7b960ad3400e57d3ad359928
0mgABear <yeoyi94@gmail.com>
2026-06-21 03:11:09 +0800

    update bot.py

--- bot.py
+++ bot.py
@@ -475,6 +475,7 @@ def main():
         app.run_webhook(
             listen="0.0.0.0",
             port=8443,
+            url_path="",
             webhook_url=WEBHOOK_URL,
             bootstrap_retries=-1
         )
