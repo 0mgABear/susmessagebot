b14ed00aacc0a2afca86e6483c1de95a4677e6fe
YY <106061169+0mgABear@users.noreply.github.com>
2026-07-05 01:02:44 +0800

    Add BAN example via HITL feedback

--- seeds.py
+++ seeds.py
@@ -199,6 +199,7 @@ examples = [
     ('Some Singaporean go Jb better behave fucking entitled idiot bitch', "SAFE"),
     ('@So_p_h_i_aa\n\n\n\n\n\n@So_p_h_i_aa', "BAN"),
     ('@So_p_h_i_aa', "BAN"),
+    ('Any 1 wanna wrk? 4.3k quick payout pm', "BAN"),
 ]
 
 
