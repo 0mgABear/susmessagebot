f28cd7f788fac1d9cc8ff4f3b6f6a1bef69c7e46
YY <106061169+0mgABear@users.noreply.github.com>
2026-07-18 19:57:10 +0800

    Add SAFE example via HITL feedback

--- seeds.py
+++ seeds.py
@@ -215,6 +215,7 @@ examples = [
     ('Come on, run and join the viral group 🔞FULL VIDEO 18+Singapore Link is in bio, join now before the link expires', "BAN"),
     ('Pm now available', "BAN"),
     ('@A_I_A_LLY\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n@A_I_A_LLY', "BAN"),
+    ('cld u contact @Kenwei021201 and meet him near tembu? this is my friends but i’m not in utown to fetch it for him', "SAFE"),
 ]
 
 
