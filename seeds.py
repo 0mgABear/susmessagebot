9e0b7b18c65fcb3c92d3aab4633d4b8b6379cb6c
YY <106061169+0mgABear@users.noreply.github.com>
2026-07-19 15:32:59 +0800

    Add BAN example via HITL feedback

--- seeds.py
+++ seeds.py
@@ -218,6 +218,7 @@ examples = [
     ('cld u contact @Kenwei021201 and meet him near tembu? this is my friends but i’m not in utown to fetch it for him', "SAFE"),
     ('I was surprised at how fast i got my profit back, I invested ₹1000 and I got a profit of ₹8,200 in lees than 6hours. She is a real professional and an honest one. All thanks to miss Karen Skidmore \n@earn_big_with_karen_skidmore', "BAN"),
     ('Pm now available for Mg fls', "BAN"),
+    ('@A_I_A_LLY', "BAN"),
 ]
 
 
