5f67ab47c25b5f7e208f5ff66c678892408e6654
YY <106061169+0mgABear@users.noreply.github.com>
2026-07-30 21:42:59 +0800

    Add BAN example via HITL feedback

--- seeds.py
+++ seeds.py
@@ -236,6 +236,7 @@ examples = [
     ('All service provided❤️😮\u200d💨\nLegit svc🍒😻\nPm for nudes 🍌🍊\nSex video content 🍒😋\nVip premium channel🍑💋\nFree girl booking😍👙\n\nJoin now on the private channel the link is in my bio', "BAN"),
     ('hello， I lost a pocket. It include bank card, Chinese id card student card. My name is Li jing. Please contact me 85988244', "SAFE"),
     ('I can’t believe how much I’ve earned with Mrs. Tina’s help. Over the last few months, my forex and crypto portfolio has grown by $12,000. It’s all thanks to her support and expertise!', "BAN"),
+    ('Any1 wan quick fast 4.8k pm', "BAN"),
 ]
 
 
