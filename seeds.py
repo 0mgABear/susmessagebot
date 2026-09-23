8e15ffac19f55213a9630df020085a68bf2713bb
YY <106061169+0mgABear@users.noreply.github.com>
2026-09-23 21:18:24 +0800

    Add BAN example via HITL feedback

--- seeds.py
+++ seeds.py
@@ -343,6 +343,7 @@ examples = [
     ('Any ppl need 3700sgd pm now @janelira if interested', "BAN"),
     ('JB ➡️ SG work opportunity. Boys & girls welcome. Serious people only. PM @mightJR96 for details.', "BAN"),
     ('DON’T SCROLL PASS THIS‼️‼️‼️\n\nDaily job opportunity \nAvailable for only Singaporeans \n$180\nPM TO KNOW HOW', "BAN"),
+    ('@C_kilty\n\n\n\n\n\n\n\n\n           \n\n\n               @C_kilty\n\n\n\n\n\n\n\n\n\n\n\n@C_kilty', "BAN"),
 ]
 
 
