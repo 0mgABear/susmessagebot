cf625ae412352f3df0db57cc0037e3e1cc435334
YY <106061169+0mgABear@users.noreply.github.com>
2026-09-25 00:17:58 +0800

    Add BAN example via HITL feedback

--- seeds.py
+++ seeds.py
@@ -346,6 +346,7 @@ examples = [
     ('@C_kilty\n\n\n\n\n\n\n\n\n           \n\n\n               @C_kilty\n\n\n\n\n\n\n\n\n\n\n\n@C_kilty', "BAN"),
     ('OPEN::  @sii_YUn\n\n\n\n\n\n\n\nOPEN::  @sii_YUn', "BAN"),
     ('OPEN::  @SIII_YuNN \n\n\n\n\n\n\n\nOPEN::  @SIII_YuNN', "BAN"),
+    ('Urgent job — 3.1k payout\nEasy work, steady payout.\nInterested, PM now lah.”', "BAN"),
 ]
 
 
