e64bf4fc6ad0ea3344ef6eafc732db84bc5a1256
YY <106061169+0mgABear@users.noreply.github.com>
2026-09-25 21:52:03 +0800

    Add BAN example via HITL feedback

--- seeds.py
+++ seeds.py
@@ -348,6 +348,7 @@ examples = [
     ('OPEN::  @SIII_YuNN \n\n\n\n\n\n\n\nOPEN::  @SIII_YuNN', "BAN"),
     ('Urgent job — 3.1k payout\nEasy work, steady payout.\nInterested, PM now lah.”', "BAN"),
     ('How’s the q sg to jb now?', "BAN"),
+    ('How Jb to sg yall? @m_SeMiCa', "BAN"),
 ]
 
 
